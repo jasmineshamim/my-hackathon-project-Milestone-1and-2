1fe7424dae3bcff4916f90cce238968552b02f94
jasmine sheikh <169073734+jasmineshamim@users.noreply.github.com>
2024-09-25 20:45:38 +0500

    Add files via upload

---
+++ milestone-4/script.ts
@@ -0,0 +1,45 @@
+// Get references to the form and display area
+const form = document.getElementById('resume-form') as HTMLFormElement;
+const ResumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
+
+// Handle form submission
+form.addEventListener('submit', (event: Event) => {
+event.preventDefault(); //Prevent page reload
+
+    // Collect input values
+
+    const name = (document.getElementById ('name') as HTMLInputElement) .value;
+    const email = (document.getElementById ('email') as HTMLInputElement) .value;
+    const phone = (document.getElementById ('phone') as HTMLInputElement) .value;
+    const education = (document.getElementById ('education') as HTMLInputElement) .value;
+    const experience = (document.getElementById ('experience') as HTMLInputElement) .value;
+    const skills = (document.getElementById ('skills') as HTMLInputElement) .value;
+
+    // Generate the resume content dynamically
+    const resumeHTML =`
+    <h2><b>Editable Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b> <span contentaditable = "true"> ${name}</span></p>
+    <p><b>Email:</b> <span contentaditable = "true"> ${email}</span></p>
+    <p><b>Phone:</b> <span contentaditable = "true"> ${phone}</span></p>
+
+
+    <h3>Education:</h3>
+    <p contentaditable = "true">${education}</p>
+
+    <h3>Experience:</h3>
+    <p contentaditable = "true">${experience}</p>
+    
+    <h3>Skills:</h3>
+    <p contentaditable = "true">${skills}</p>
+
+    `;
+    // Display the generated resume
+    if(ResumeDisplayElement){
+        ResumeDisplayElement.innerHTML = resumeHTML;
+    }else{
+        console.error('The resume display element is missing.');
+    }
+
+
+});
